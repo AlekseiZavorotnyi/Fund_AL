877e06f48d7888a94d0c0ade148fbdd5f1b489e2
AlekseiZavorotnyi <zavorotny2005@gmail.com>
2025-04-28 23:34:09 +0300

    commit lab 1_4

--- Lab1_4/tests/testStack.cpp
+++ Lab1_4/tests/testStack.cpp
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
-#include "stack.h"
+#include "Stack.h"
 #include <algorithm>
 #include <string>
+#include <vector>
 
 using namespace my_cont;
 
@@ -17,6 +18,7 @@ protected:
     Stack<int> stackWithValues{1, 2, 3, 4, 5};
 };
 
+// Конструкторы и операторы присваивания
 TEST_F(StackTest, DefaultConstructor) {
     EXPECT_TRUE(emptyStack.empty());
     EXPECT_EQ(emptyStack.size(), 0);
@@ -25,24 +27,18 @@ TEST_F(StackTest, DefaultConstructor) {
 TEST_F(StackTest, InitializerListConstructor) {
     EXPECT_FALSE(stackWithValues.empty());
     EXPECT_EQ(stackWithValues.size(), 5);
-
     EXPECT_EQ(stackWithValues.top(), 5);
 }
 
 TEST_F(StackTest, CopyConstructor) {
     Stack<int> copy(stackWithValues);
-
     EXPECT_EQ(copy.size(), stackWithValues.size());
     EXPECT_EQ(copy.top(), stackWithValues.top());
-
-    stackWithValues.pop();
-    EXPECT_EQ(copy.size(), 5);
 }
 
 TEST_F(StackTest, MoveConstructor) {
     Stack<int> original{10, 20, 30};
     Stack<int> moved(std::move(original));
-
     EXPECT_TRUE(original.empty());
     EXPECT_EQ(moved.size(), 3);
     EXPECT_EQ(moved.top(), 30);
@@ -51,34 +47,43 @@ TEST_F(StackTest, MoveConstructor) {
 TEST_F(StackTest, CopyAssignment) {
     Stack<int> copy;
     copy = stackWithValues;
-
     EXPECT_EQ(copy.size(), stackWithValues.size());
     EXPECT_EQ(copy.top(), stackWithValues.top());
-
-    copy = copy;
-    EXPECT_EQ(copy.size(), 5);
 }
 
 TEST_F(StackTest, MoveAssignment) {
     Stack<int> original{10, 20, 30};
     Stack<int> moved;
     moved = std::move(original);
-
     EXPECT_TRUE(original.empty());
     EXPECT_EQ(moved.size(), 3);
     EXPECT_EQ(moved.top(), 30);
 }
 
+// Основные операции
 TEST_F(StackTest, Top) {
     EXPECT_EQ(stackWithValues.top(), 5);
 
-    stackWithValues.push(0);
-    EXPECT_EQ(stackWithValues.top(), 0);
-
+    stackWithValues.push(6);
+    EXPECT_EQ(stackWithValues.top(), 6);
 }
 
 TEST_F(StackTest, TopThrowWhenEmpty) {
     EXPECT_THROW(emptyStack.top(), std::out_of_range);
+}
+
+TEST_F(StackTest, PushAndPop) {
+    stackWithValues.push(6);
+    EXPECT_EQ(stackWithValues.top(), 6);
+    EXPECT_EQ(stackWithValues.size(), 6);
+
+    stackWithValues.pop();
+    EXPECT_EQ(stackWithValues.top(), 5);
+    EXPECT_EQ(stackWithValues.size(), 5);
+}
+
+TEST_F(StackTest, PopThrowWhenEmpty) {
+    EXPECT_THROW(emptyStack.pop(), std::out_of_range);
 }
 
 TEST_F(StackTest, EmptyAndSize) {
@@ -89,18 +94,11 @@ TEST_F(StackTest, EmptyAndSize) {
     EXPECT_EQ(stackWithValues.size(), 5);
 }
 
-TEST_F(StackTest, PushPop) {
-    stackWithValues.push(0);
-    EXPECT_EQ(stackWithValues.top(), 0);
-    EXPECT_EQ(stackWithValues.size(), 6);
-
-    stackWithValues.pop();
-    EXPECT_EQ(stackWithValues.top(), 5);
-    EXPECT_EQ(stackWithValues.size(), 5);
-}
-
-TEST_F(StackTest, PopEmptyThrows) {
-    EXPECT_THROW(emptyStack.pop(), std::out_of_range);
+TEST_F(StackTest, Clear) {
+    EXPECT_FALSE(stackWithValues.empty());
+    stackWithValues.clear();
+    EXPECT_TRUE(stackWithValues.empty());
+    EXPECT_EQ(stackWithValues.size(), 0);
 }
 
 TEST_F(StackTest, Swap) {
@@ -114,14 +112,10 @@ TEST_F(StackTest, Swap) {
     EXPECT_EQ(other.top(), 5);
 }
 
+// Операторы сравнения
 TEST_F(StackTest, ComparisonOperators) {
     Stack<int> same{1, 2, 3, 4, 5};
     Stack<int> smaller{1, 2, 3};
-    Stack<int> smaller2{1, 2, 2};
-    Stack<int> greater{1, 2, 3, 4, 5, 6};
-    Stack<int> greater2{1, 2, 4};
-    Stack<int> different{5, 4, 3, 2, 1};
-
 
     EXPECT_TRUE(stackWithValues == same);
     EXPECT_FALSE(stackWithValues == smaller);
@@ -130,34 +124,17 @@ TEST_F(StackTest, ComparisonOperators) {
     EXPECT_FALSE(stackWithValues != same);
 
     EXPECT_TRUE(smaller < stackWithValues);
-    EXPECT_TRUE(smaller2 < stackWithValues);
     EXPECT_FALSE(stackWithValues < smaller);
-    EXPECT_FALSE(stackWithValues < same);
 
     EXPECT_TRUE(smaller <= stackWithValues);
-    EXPECT_TRUE(stackWithValues <= same);
-    EXPECT_FALSE(stackWithValues <= smaller2);
-
-    EXPECT_TRUE(stackWithValues > smaller);
-    EXPECT_TRUE(greater > stackWithValues);
-    EXPECT_TRUE(greater2 > stackWithValues);
-    EXPECT_FALSE(smaller > stackWithValues);
-    EXPECT_FALSE(stackWithValues > same);
-
     EXPECT_TRUE(stackWithValues >= smaller);
-    EXPECT_TRUE(stackWithValues >= same);
-    EXPECT_FALSE(smaller >= stackWithValues);
 
     EXPECT_EQ((stackWithValues <=> same), std::strong_ordering::equal);
     EXPECT_EQ((stackWithValues <=> smaller), std::strong_ordering::greater);
-    EXPECT_EQ((smaller <=> stackWithValues), std::strong_ordering::less);
-    EXPECT_EQ((stackWithValues <=> greater), std::strong_ordering::less);
-    EXPECT_EQ((greater <=> stackWithValues), std::strong_ordering::greater);
-    EXPECT_EQ((stackWithValues <=> smaller2), std::strong_ordering::greater);
-    EXPECT_EQ((smaller2 <=> stackWithValues), std::strong_ordering::less);
 }
 
-TEST_F(StackTest, SingleElementList) {
+// Краевые случаи
+TEST_F(StackTest, SingleElementStack) {
     Stack<int> single{42};
 
     EXPECT_EQ(single.size(), 1);
@@ -169,11 +146,31 @@ TEST_F(StackTest, SingleElementList) {
     single.push(100);
     EXPECT_EQ(single.size(), 1);
     EXPECT_EQ(single.top(), 100);
-
-    single.pop();
-    EXPECT_TRUE(single.empty());
 }
 
+TEST_F(StackTest, LargeStack) {
+    const int largeSize = 10000;
+    Stack<int> largeStack;
+
+    for (int i = 0; i < largeSize; ++i) {
+        largeStack.push(i);
+    }
+
+    EXPECT_EQ(largeStack.size(), largeSize);
+    EXPECT_EQ(largeStack.top(), largeSize - 1);
+
+    largeStack.clear();
+    EXPECT_TRUE(largeStack.empty());
+}
+
+TEST_F(StackTest, DifferentContainerType) {
+    // Тест с другим контейнером (Deque по умолчанию)
+    Stack<int, Deque<int>> dequeStack;
+    dequeStack.push(1);
+    dequeStack.push(2);
+    EXPECT_EQ(dequeStack.top(), 2);
+    EXPECT_EQ(dequeStack.size(), 2);
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
